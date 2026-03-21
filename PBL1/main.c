cbc04828de13c6f01f99312dae0e2d13aa678490
luongvuong2k7 <luongvuongqn07@gmail.com>
2026-03-21 09:43:04 +0700

    Refactor main.c for improved structure and clarity

--- PBL1/main.c
+++ PBL1/main.c
@@ -4,51 +4,61 @@
 #define TRUE 1
 #define FALSE 0
 #define NOT_FOUND -1
+#define MAX_SIZE 50
+#define MAX_SV 100
+
 typedef unsigned int Position;
 typedef unsigned int Quantity;
 
-char l_name[5001];
-char f_name[5001];
-char birthday[5001];
-int sex[101];
-char address[5001];
-char email[5001];
-int id[101];
+char l_name[MAX_SV*MAX_SIZE];
+char f_name[MAX_SV*MAX_SIZE];
+char birthday[MAX_SV*MAX_SIZE];
+int sex[MAX_SIZE];
+char address[MAX_SV*100];
+char email[MAX_SV*MAX_SIZE];
+int id[MAX_SIZE];
 Position count=0;
+
+int isFull(Quantity count)
+{
+	return (count==MAX_SV)?TRUE:FALSE;
+}
+
+int isEmpty(Quantity count)
+{
+	return (count==0)?TRUE:FALSE;
+}
 
 int get_data(Position *count, char *l_name, char *f_name, char *birthday, int *sex, char *address, int *id, char *email)
 {
 	FILE *file = fopen("text.txt","r");
 	if (file==NULL) return FALSE;
-	while (fscanf(file, "%49[^:]:%49[^:]:%49[^:]:%d:%49[^:]:%u:%49s\n",
-		l_name   +*count*50,
-		f_name   +*count*50,
-		birthday +*count*50,
+	while (fscanf(file, "%49[^:]:%49[^:]:%49[^:]:%d:%49[^:]:%d:%49s\n",
+		l_name   +*count*MAX_SIZE,
+		f_name   +*count*MAX_SIZE,
+		birthday +*count*MAX_SIZE,
 		sex      +*count,
-		address  +*count*50,
+		address  +*count*100,
 		id       +*count,
-		email    +*count*50) == 7) (*count)++;
+		email    +*count*MAX_SIZE) == 7) (*count)++;
 	fclose(file);
 	return TRUE;
 }
 
 void show(Quantity count, char *l_name, char *f_name, char *birthday, int *sex, char*address, int id[], char *email)
 {
+	if (isEmpty(count)) printf("Hiện chưa có thông tin.\n");
 	int i;
 	for(i=0; i<count; i++)
 	{
-		printf("Họ và Tên: %s %s\n",l_name+i*50, f_name+i*50);
+		printf("Họ và Tên: %s %s\n",l_name+i*MAX_SIZE, f_name+i*MAX_SIZE);
 		printf("Giới tính: %s\n",(sex[i])?"Nam":"Nữ");
-		printf("ID: %u\n",id[i]);
-		printf("Địa chỉ: %s\n",address+i*50);
-		printf("Email: %s\n",email+i*50);
-		printf("-------------\n");
+		if (id[i]==-1) printf("ID: Chưa có\n");
+		else printf("ID: %d\n",id[i]);
+		printf("Địa chỉ: %s\n",address+i*100);
+		printf("Email: %s\n",((strcmp(email+i*MAX_SIZE,"none")==0)?"Chưa có":email+i*MAX_SIZE));
 	}
-}
-
-int isFull(int count)
-{
-	return (count==100)?TRUE:FALSE;
+	printf("-------------\n");
 }
 
 int add(Position *count, char *l_name, char *f_name, char *birthday, int *sex, char *address, int *id, char *email)
@@ -65,18 +75,18 @@ int add(Position *count, char *l_name, char *f_name, char *birthday, int *sex, c
 		i++;
 		printf("Hãy nhập thông tin người thứ %d\n",i);
 		printf("Họ lót: ");
-		scanf(" %49[^\n]",l_name+*count*50);
+		scanf(" %49[^\n]",l_name+*count*MAX_SIZE);
 		printf("Tên: ");
-		scanf(" %49[^\n]",f_name+*count*50);
-		printf("Sinh nhật: ");
-		scanf(" %49[^\n]",birthday+*count*50);
+		scanf(" %49[^\n]",f_name+*count*MAX_SIZE);
+		printf("Sinh nhật (VD:02/09/1945): ");
+		scanf(" %49[^\n]",birthday+*count*MAX_SIZE);
 		printf("Giới tính (Nam:1 | Nữ:0): ");
 		scanf("%d",&sex[*count]);
-		printf("Địa chỉ: ");
-		scanf(" %49[^\n]",address+*count*50);
+		printf("Địa chỉ : ");
+		scanf(" %49[^\n]",address+*count*100);
 
 		id[*count] = -1;
-		strcpy(email +*count*50,"none");
+		strcpy(email +*count*MAX_SIZE,"none");
 
 		(*count)++;
 		if (isFull(*count)) return FALSE;
@@ -90,15 +100,14 @@ void save(Quantity *count, char *l_name, char *f_name, char *birthday, int *sex,
 	int i;
 	for(i=0; i<*count; i++)
 	{
-
 		fprintf(file,"%s:%s:%s:%d:%s:%d:%s\n",
-		l_name   + i*50,
-		f_name   + i*50,
-		birthday + i*50,
+		l_name   + i*MAX_SIZE,
+		f_name   + i*MAX_SIZE,
+		birthday + i*MAX_SIZE,
 		sex[i],
-		address  + i*50,
+		address  + i*100,
 		id[i],
-		email    + i*50
+		email    + i*MAX_SIZE
 		);
 	}
 	fclose(file);
@@ -117,7 +126,6 @@ void menu()
 		return;
 	}
 	show(count,l_name,f_name,birthday,sex,address,id,email);
-	printf("\n---\n");
 	printf("Menu\n");
 	printf("1-Thêm sinh viên\n");
 	printf("2-Sắp xắp danh sách\n");
@@ -146,7 +154,6 @@ void menu()
 		{
 			return;
 		}
-
 	}
 }
 
